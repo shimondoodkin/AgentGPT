3664d7de8b720345bda7e5e8608e6dd8182323f0
Shimon Doodkin <helpmepro1@gmail.com>
2023-04-13 04:35:46 +0300

    add setup.sh --devdocker ,( and --skipenv)

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 cd "$(dirname "$0")" || exit
 
+if [ "$1" = "--skipenv" ]; then
+
 echo -n "Enter your OpenAI Key (eg: sk...): "
 read OPENAI_API_KEY
 
@@ -8,19 +10,22 @@ NEXTAUTH_SECRET=$(openssl rand -base64 32)
 
 ENV="NODE_ENV=development\n\
 NEXTAUTH_SECRET=$NEXTAUTH_SECRET\n\
-NEXTAUTH_URL=http://localhost:3000\n\
+NEXTAUTH_URL=https://agentgpt.doodkin.com\n\
 OPENAI_API_KEY=$OPENAI_API_KEY\n\
 DATABASE_URL=file:../db/db.sqlite\n"
-
 printf $ENV > .env
 
+fi
+
 if [ "$1" = "--docker" ]; then
-  printf $ENV > .env.docker
+  cp .env .env.docker
   docker build -t agentgpt .
-  docker run -d --name agentgpt -p 3000:3000 -v $(pwd)/db:/app/db agentgpt
+  docker run -d --name agentgpt -p 10280:3000 -v $(pwd)/db:/app/db agentgpt
+elif [ "$1" = "--devdocker" ]; then
+  docker run -dti --name agentgpt -p 10280:3000 -v $(pwd)/:/app/ node:bullseye bash /app/setup.sh --skipenv
 else
-  printf $ENV > .env
   ./prisma/useSqlite.sh
   npm install
   npm run dev
 fi
+
